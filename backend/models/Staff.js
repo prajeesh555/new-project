0671a6ed2fc59d0aecf147d317d9e0bc37036e25
prajeesh555 <prajeeshspc2002@gmai.com>
2025-11-26 14:50:58 +0530

    Updated all backend and frontend code

--- backend/models/Staff.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const staffSchema = new mongoose.Schema({
-  name: String,
-  role: String,
-  phone: String
-});
-
-export default mongoose.model("Staff", staffSchema);
--- backend/models/User.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true }
-});
-
-export default mongoose.model("User", userSchema);
